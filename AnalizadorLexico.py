e44916817a715aa9655c40d35b73c25f5a72bccf
emeliForero <91347149+emeliForero@users.noreply.github.com>
2024-03-21 02:56:11 +0000

    Se agregan funciones y logica para finalizar el analizador sintactico

--- AnalizadorLexico.py
+++ AnalizadorLexico.py
@@ -14,7 +14,15 @@ class Lexer:
         'OPERADOR',
         'CADENA',
         'SIMBOLO_ESPECIAL',
-        'COMENTARIO'
+        'COMENTARIO',
+        'MAS',
+        'MENOS',
+        'POR',
+        'DIVIDIDO',
+        'DEF',
+        'IF',
+        'NEWLINE',
+        'COMENTARIO_MULTILINEA'
     ]
 
     # Expresiones regulares  
@@ -22,9 +30,17 @@ class Lexer:
     t_IDENTIFICADOR = r'[a-zA-Z_][a-zA-Z0-9_]*'
     t_NUMERO = r'\d+'
     t_OPERADOR = r'[+\-*/=]'
-    t_CADENA = r'\"[^\"]*\"'
-    t_SIMBOLO_ESPECIAL = r'[(),:;{}<>[\]]'
+    t_CADENA = r'"[^"]*"'
+    t_SIMBOLO_ESPECIAL = r'[(),:;{}<=>\[\]]'
     t_COMENTARIO = r'\#.*'
+    t_MAS = r'\+'
+    t_MENOS = r'-'
+    t_POR = r'\*'
+    t_DIVIDIDO = r'/'
+    t_DEF = r'def'
+    t_IF = r'if'
+    t_NEWLINE = r'\n+'
+    t_COMENTARIO_MULTILINEA = r'/\*(.|\n)*?\*/'
 
     # Ignorar espacios
     t_ignore = ' '
@@ -33,11 +49,6 @@ class Lexer:
     def t_error(self, t):
         print(f"Carácter no reconocido: '{t.value[0]}' en la línea {t.lineno}")
         t.lexer.skip(1)
-
-    # Saltos de línea
-    def t_newline(self, t):
-        r'\n+'
-        t.lexer.lineno += len(t.value)
 
     def build(self, **kwargs):
         self.lexer = lex.lex(module=self, **kwargs)
@@ -48,17 +59,15 @@ class Lexer:
     def analizar_archivo(self, nombre_archivo):
         with open(nombre_archivo, 'r') as archivo:
             codigo = archivo.read()
-            self.lexer.input(codigo)    
+            self.lexer.input(codigo)
 
 lexer = Lexer()
 lexer.analizar_archivo("codigo.txt")
-
 analizador = lexer.lexer
 
-#Tokens encontrados
+# Tokens encontrados
 def token_final():
     for token in analizador:
         token_obj = AnalizadorLexico(token.type, token.value, token.lineno)
-        #print(f'Tipo: {token_obj.tipo}, Valor: {token_obj.valor}, Línea: {token_obj.linea}')
         print(f'{token_obj.tipo}: {token_obj.valor}')
     return analizador
--- AnalizadorSintactico.py
+++ AnalizadorSintactico.py
@@ -1,26 +1,121 @@
 import ply.yacc as yacc
-from AnalizadorLexico import Lexer, AnalizadorLexico
+from AnalizadorLexico import Lexer
 
-#https://docs.python.org/es/3.9/library/html.parser.html?highlight=sintactico
+class LlamadaFuncion:
+    def __init__(self, nombre, argumentos):
+        self.nombre = nombre
+        self.argumentos = argumentos
 
 class AnalizadorSintactico:
-     
     tokens = Lexer.tokens
-    
+
     precedence = (
-        ('left','MAS','MENOS'),
-        ('left','POR','DIVIDIDO'),
-        ('right','UMENOS'),
-        )
-    
-    def p_expresion_evaluar(t):
-     'expresion : expresion MAS expresion'
-     #   ^            ^      ^    ^
-     #  t[0]         t[1]   t[2] t[3]
- 
-     t[0] = t[1] + t[3]
+        ('left', 'MAS', 'MENOS'),
+        ('left', 'POR', 'DIVIDIDO'),
+        ('right', 'UMENOS'),
+    )
 
+    def p_expresion_evaluar_binaria(self, t):
+        '''expresion : expresion MAS expresion
+                     | expresion MENOS expresion
+                     | expresion POR expresion
+                     | expresion DIVIDIDO expresion'''
+        if len(t) == 4:  # Si se trata de una operación binaria
+            if t[2] == '+':
+                t[0] = t[1] + t[3]
+            elif t[2] == '-':
+                t[0] = t[1] - t[3]
+            elif t[2] == '*':
+                t[0] = t[1] * t[3]
+            elif t[2] == '/':
+                if t[3] == 0:
+                    raise ZeroDivisionError("División por cero")
+                t[0] = t[1] / t[3]
 
+    def p_expresion_umenos(self, t):
+        'expresion : MENOS expresion %prec UMENOS'
+        t[0] = -t[2]
 
+    def p_expresion_numero_identificador(self, t):
+        '''expresion : NUMERO
+                     | IDENTIFICADOR
+                     | CADENA
+                     | PALABRA_RESERVADA'''
+        t[0] = t[1]
 
+    def p_expresion_parentesis(self, t):
+        'expresion : "(" expresion ")"'
+        t[0] = t[2]
 
+    def p_instruccion_funcion(self, t):
+        '''instruccion : DEF IDENTIFICADOR '(' parametros ')' ':' cuerpo_funcion'''
+        pass
+
+    def p_parametros(self, t):
+        '''parametros : IDENTIFICADOR
+                      | parametros ',' IDENTIFICADOR'''
+        pass
+
+    def p_cuerpo_funcion(self, t):
+        '''cuerpo_funcion : instrucciones'''
+        pass
+
+    def p_expresion_condicional(self, t):
+        '''expresion : IF expresion ':' cuerpo_condicional'''
+        pass
+
+    def p_cuerpo_condicional(self, t):
+        '''cuerpo_condicional : instrucciones'''
+        pass
+
+    def p_expresion_llamada_funcion(self, t):
+        '''expresion : IDENTIFICADOR '(' argumentos ')' '''
+        t[0] = LlamadaFuncion(t[1], t[3])
+
+    def p_argumentos(self, t):
+        '''argumentos : expresion
+                      | argumentos ',' expresion'''
+        if len(t) == 2:
+            t[0] = [t[1]]
+        else:
+            t[0] = t[1] + [t[3]]
+
+    def p_comentario(self, t):
+        '''comentario : COMENTARIO
+                    | COMENTARIO_MULTILINEA'''
+        pass
+
+    def p_instrucciones(self, t):
+        '''instrucciones : instruccion
+                        | comentario
+                        | instrucciones instruccion
+                        | instrucciones comentario'''
+        if len(t) == 2:
+            t[0] = [t[1]]
+        else:
+            t[0] = t[1] + [t[2]]        
+
+    def p_error(self, t):
+        if t:
+            print(f"Error de sintaxis en la línea {t.lineno}: Token inesperado '{t.value}'")
+        else:
+            print("Error de sintaxis al final del archivo")
+        self.parser.restart()
+
+    def __init__(self):
+        # Crear una instancia de la clase Lexer
+        lexer = Lexer()
+        # Ejecutar el análisis léxico con el archivo "codigo.txt"
+        lexer.analizar_archivo("codigo.txt")
+        # Obtener el lexer generado
+        analizador_lexico = lexer.lexer
+        # Crear el parser yacc
+        self.parser = yacc.yacc(module=self)
+        # Definir el código a analizar
+        with open("codigo.txt", "r") as archivo:
+            codigo = archivo.read()
+        # Ejecutar el análisis sintáctico con el código dado
+        self.parser.parse(codigo)
+
+# Crear una instancia de AnalizadorSintactico para ejecutarlo
+AnalizadorSintactico()
